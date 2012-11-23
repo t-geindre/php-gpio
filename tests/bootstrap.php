26c0bac33627bffc0687e18c0fb5607e05f0c383
Ronan Guilloux <ronan.guilloux@gmail.com>
2012-11-23 10:45:32 +0100

    warning for non-raspberry-pi machines

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,10 +1,22 @@
 <?php
 
+$rpi = 'raspberrypi';
+if ($rpi !== $nodename = exec('uname --nodename')) {
+    $warning = sprintf("Precondition is not met : %s is not a %s machine! ", $nodename, $rpi);
+    echo <<<EOT
+
+$warning
+
+
+EOT;
+}
+
+
 if (!extension_loaded('curl') || !function_exists('curl_init')) {
     die(<<<EOT
 cURL has to be enabled!
 EOT
-    );
+);
 }
 
 if (!($loader = @include __DIR__ . '/../vendor/autoload.php')) {
@@ -17,7 +29,7 @@ $ php composer.phar install --dev
 $ phpunit
 
 EOT
-    );
+);
 }
 
 $loader->add('PhpGpio\Tests', __DIR__);
